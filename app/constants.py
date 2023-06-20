988699b3bbe0d3cadb1c4025c8a649f24097285f
dalbezh <dalbezh@icloud.com>
2023-06-20 20:32:50 +0300

    refactoring

--- app/constants.py
+++ app/constants.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PWD_HASH_SALT = os.environ.get("PWD_HASH_SALT")
-PWD_HASH_ITERATIONS = os.environ.get("PWD_HASH_ITERATIONS")
+PWD_HASH_SALT = str.encode(os.environ.get("PWD_HASH_SALT"))
+PWD_HASH_ITERATIONS = int(os.environ.get("PWD_HASH_ITERATIONS"))
 JWT_SECRET = os.environ.get("JWT_SECRET")
 JWT_ALGORITHM = os.environ.get("JWT_ALGORITHM")
